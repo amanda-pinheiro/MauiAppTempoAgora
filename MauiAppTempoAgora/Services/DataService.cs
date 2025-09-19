7ece551549f32a9e6c04fbfc032bdadb1a689523
AmandaPinheiro <amandadejesuspinheiro@outlook.com>
2025-09-18 22:35:54 -0300

    comunicação com API

---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,56 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "ce5ac41c46a5e251d991e398f400fb17";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient cliente = new HttpClient())
+            { 
+                HttpResponseMessage resp = await cliente.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lat"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+
+                    };
+                }
+
+                
+                
+            
+            }
+
+            return t;
+
+        }
+    }
+}
